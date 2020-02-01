7363efa6019f2a48caab9c8b91b323cc6f439257
alejandrosanchezhernandez <alejandrosanchezhernandez@gmail.com>
2020-02-01 03:48:40 +0100

    ok
    ok

---
+++ Assets/_GameAssets/Scripts/GiroscopioCamara.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiroscopioCamara : MonoBehaviour
+{
+    public GameObject camaraVr;
+    public bool seIniciJuego;
+
+    private float posInicialY = 0f;
+    private float posInicialGiroY = 0f;
+    private float calibrarPosY = 0f;
+
+    void Start()
+    {
+        Input.gyro.enabled = true;
+        posInicialY = camaraVr.transform.eulerAngles.y;
+    }
+
+    void Update()
+    {
+        AplicarRotacionGiroscopio();
+        AplicarCalibracion();
+
+        if(seIniciJuego == true)
+        {
+            Invoke("CalibrarPosicionY", 3f);
+            seIniciJuego = false;
+        }
+    }
+
+    void AplicarRotacionGiroscopio()
+    {
+        camaraVr.transform.rotation = Input.gyro.attitude;
+        camaraVr.transform.Rotate(0f, 0f, 180f, Space.Self);
+        camaraVr.transform.Rotate(90f, 180f, 0f, Space.World);
+        posInicialGiroY = camaraVr.transform.eulerAngles.y;
+    }
+
+    void CalibrarPosicionY()
+    {
+        calibrarPosY = posInicialGiroY - posInicialY;
+    }
+
+    void AplicarCalibracion()
+    {
+        camaraVr.transform.Rotate(0f, -calibrarPosY, 0f, Space.World);
+    }
+}
